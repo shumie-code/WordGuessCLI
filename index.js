9f3492e483cb6a5b8586a9f62154626c11ef2cb7
Petros Shumie <shumie.peter@gmail.com>
2019-11-05 14:42:35 -0500

    created regex function to check for correct letter input in strings. In this case the letters a-z

---
+++ index.js
@@ -0,0 +1,5 @@
+//The RegExp test() Method in JavaScript is used to test for match in a string. If there is a match this method returns true else it returns false. Where str is the string to be searched. 
+// Here it is used to check for correct Letter input
+// the - between a-z allows for spaces in words
+var letterCheck = /^[a-z]$/
+
