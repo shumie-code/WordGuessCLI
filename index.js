b03e908aa8c1b8a96610c51f72300bb1913671a5
Petros Shumie <shumie.peter@gmail.com>
2019-11-05 16:29:27 -0500

    created play game function and set inital game logic, also set cconsole.log to display total guess and set variables for chosen words to be selected randomly from word array and the game word to be a new word on each play.

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-var Word = require("./word");
-
+// Calls word.js functions via word.js module
+var Word = require("./word.js");
+// Calls npm package Inquirer
 var inquirer = require("inquirer");
 
+function playGame() {
+    var wordArr = ["sun", "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto"];
+
+    var usedArray = [];
+
+    var letterCheck = /^[a-z]$/;
+
+    var totalGuesses = 20;
+
+    console.log("\nThe Solar System Game. There are " totalGuesses + " Attempts to guess the planets names\n");
+    var chosenWord = wordArr[Math.floor(Math.random() * wordArr.length)]
+    var gameWord = new Word(chosenWord)
+    gameWord.makeWord();
+
+    for (var i=0; i < gameWord.letterArr.length; i++) {
+        if (!letterCheck.test(gameWord.letterArr[i].letter)) {
+            console.log(gameWord.letterArr[i].letter);
+            gameWord.letterArr[i].guessed = true;
+        }
+    }
+
+    gameWord.dispWord()
+
+    promptUser();
+
+    function promptUser() {
+        inquirer.prompt
+    }
+}
