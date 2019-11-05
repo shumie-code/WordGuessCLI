edc2dfcdb3d365d2adf0a35d552604de686b56b5
Petros Shumie <shumie.peter@gmail.com>
2019-11-05 15:37:04 -0500

    created constructor function that calls to the constructor from letter.js to create an object representing an array of new letter objects representing the letters of the underlying word.

--- word.js
+++ word.js
@@ -1,2 +1,17 @@
 // Require called with fs package to grab functions from letter.js using modularzation.
 var letter = require("./letter.js");
+// Constructor function that depends on the letter constructor
+function Word(char) {
+    //this is used to create an ob
+    this.getWord = char;
+    this.letterArr =[];
+    this.makeWord = function() {
+        var gameArr = this.getWord.split("");
+        for (var i=0; i < gameArr.length; i++) {
+            this.letterArr.push(new letter(gameArr[i]));
+        }
+    };
+
+
+
+}
